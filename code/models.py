83e3e12d56b7eb5ed504f548ca1201e24157fd79
mani-shailesh <shailesh.pandey@iitrpr.ac.in>
2017-01-30 02:38:41 +0530

    Add implementation of required methods in Model class

--- code/models.py
+++ code/models.py
@@ -67,6 +67,7 @@ class RMLR:
                 x = X[init_idx:init_idx + batch_size, :]
                 y_c = util.to_categorical(y, self.num_classes)
                 f = self.predict_values(x)
+                # noinspection PyTypeChecker
                 del_J = np.dot(np.transpose(f - y_c), x) + \
                         (lambda_ * np.c_[np.zeros(self.num_classes), self.W[:, 1:]])
                 self.W -= (lr * del_J)
@@ -104,6 +105,7 @@ class RMLR:
         :return: list of predicted class label indices
         """
         values = self.predict_values(X)
+        # noinspection PyTypeChecker
         return np.argmax(values, axis=1)
 
 
@@ -112,7 +114,7 @@ class Model:
     Class to represent a model composed of layers
     """
 
-    def __init__(self, input_layer, output_layer):
+    def __init__(self, input_layer, output_layer, log_file):
         """
         Initialize the model
         :param input_layer: 'Layer' object to act as the input layer
@@ -129,3 +131,86 @@ class Model:
             else:
                 self.layers.append(layer)
         self.layers.reverse()
+        self.log_file = log_file
+        self.num_classes = output_layer.get_output_shape()[1]
+
+    def train(self, X, Y, loss, lr, batch_size, val_X, val_Y, num_epochs):
+        """
+        Train the model using 'X' as training data and 'Y' as labels
+        :param X: numpy array of feature vectors
+        :param Y: list of actuals label indices
+        :param loss:    Object of `Loss` layer class
+        :param lr:  learning rate float (> 0)
+        :param batch_size:  int (> 0)
+        :param num_epochs:  int (>= 0)
+        :param val_X:   numpy array of feature vectors for validation set
+                        No validation performed if this is None
+        :param val_Y:   class labels for validation set
+                        Used only if val_X is not None
+        :return Tuple of best validation accuracy, training accuracy at that epoch and the epoch no.
+        """
+
+        n = X.shape[0]
+
+        print("Starting training...")
+
+        best_val_acc = 0
+        best_train_acc = 0
+        best_epoch = 1
+
+        util.log(self.log_file, "epoch,train_acc,val_acc")
+
+        for epoch_idx in range(num_epochs):
+            print("Performing epoch #" + str(epoch_idx + 1))
+
+            # Shuffle training data and labels
+            perm = np.random.permutation(n)
+            X = X[perm, :]
+            Y = Y[perm]
+
+            for init_idx in range(0, n, batch_size):
+                y = Y[init_idx:init_idx + batch_size]
+                x = X[init_idx:init_idx + batch_size, :]
+                y_c = util.to_categorical(y, self.num_classes)
+                o = self.forward_pass(x)
+
+                # Perform back propagation on the network
+                grad = loss.get_gradient(y_c, o) * lr
+                for layer in reversed(self.layers):
+                    grad = layer.back_propagation(grad)
+
+            train_acc = util.get_accuracy(Y, self.predict_classes(X))
+            val_acc = util.get_accuracy(val_Y, self.predict_classes(val_X))
+
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                best_train_acc = train_acc
+                best_epoch = epoch_idx + 1
+
+            util.log(self.log_file,
+                     str(epoch_idx + 1) + "," + str(train_acc) + "," + str(val_acc))
+            print("Training accuracy: " + str(train_acc))
+            print("Validation accuracy: " + str(val_acc))
+            print("\n------------------------------------\n")
+
+        print("Training completed.")
+        return best_train_acc, best_val_acc, best_epoch
+
+    def forward_pass(self, X):
+        """
+        Perform forward pass of the network on given data
+        :param X: numpy array of input data N x D
+        :return: numpy array of output N x C
+        """
+        for layer in self.layers:
+            X = layer.forward_pass(X)
+        return X
+
+    def predict_classes(self, X):
+        """
+        Predict the class labels using 'X' as test data
+        :param X: numpy array of feature vectors
+        :return: list of predicted class label indices
+        """
+        values = self.forward_pass(X)
+        return np.argmax(values, axis=1)
